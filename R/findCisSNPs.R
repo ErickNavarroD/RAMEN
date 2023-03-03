8b29ce104b01793ed10d475b29c448eda87a0eb0
Erick Navarro <ericknadel98@ciencias.unam.mx>
2023-03-03 15:11:14 -0800

    simplified the returned object from a list to a single df

--- R/findCisSNPs.R
+++ R/findCisSNPs.R
@@ -4,18 +4,14 @@
 #' positions of the VMR data frame and the ones in the genotype information are from the same genome build.
 #'
 #' @param VMRs_df A data frame converted from a Genomic Ranges object. Must contain the following columns:
-#' "probes", containing a list where each element contains a vector with the probes constituting a  VMR
+#' "seqnames", "start", "end".
 #' @param genotype_information A data frame with information about genotyped sites of interest. It must contain the following
 #' columns: "Chr" - Number of chromosome, "Position" - Genomic position of the chromosome (must contain values of class int),
 #' and "Name" - ID of the SNP.
 #' @param distance The distance threshold to be used to identify cis SNPs
 #'
-#' @return a list with the following elements:
-#' - A GenomicRanges::countOverlaps() output, which is an integer vector containing the tabulated query overlap hits.
-#'  In other words, it is a data frame with two columns; the first one is the VMR number ID (which corresponds
-#'  to the order of the VMRs in the VMR_df object), and a second one with the number of cis SNPs that were found. This
-#'  is a useful object for ploting cisSNP distributions.
-#' - VMR_df: a VMR_df object but now with a new column including the cis SNPs identified for each VMR.
+#' @return a VMR_df: a VMR_df object but now with a new column including the cis SNPs identified for each VMR, and the number
+#' of SNPs surrounding each VMR in the specified window
 #' @export
 
 findCisSNPs = function(VMRs_df, genotype_information, distance = 1e6){
@@ -29,20 +25,21 @@ findCisSNPs = function(VMRs_df, genotype_information, distance = 1e6){
                               names = genotype_information$Name))
   #Extend each VMR 1 Mb up and downstream
   VMRs_extended = VMRs_gr + distance
-  #### Get the number of overlaps per extended VMR ####
-  snps_per_vmr_counts = data.frame(VMR_index = c(1:length(VMRs_extended)),
-                                   number_of_SNPs =  GenomicRanges::countOverlaps(VMRs_extended, genot_gr))
 
-  ####Identify the SNPs that are present in each VMR ####
-  snps_per_vmr_find =  GenomicRanges::findOverlaps(VMRs_extended, genot_gr, select = "all")
-  rownames(genotype_information) = genotype_information$Name
-  VMRs_df_with_cisSNPs = VMRs_df %>%
-    dplyr::mutate(SNP = sapply(snps_per_vmr_find, map_revmap_names, genotype_information))
+  VMRs_df_with_cisSNPs = VMRs_df
   if(!"VMR_index" %in% colnames(VMRs_df_with_cisSNPs)){ # Add a VMR index to each region if not already existing
     VMRs_df_with_cisSNPs = VMRs_df_with_cisSNPs %>%
       tibble::rownames_to_column(var = "VMR_index")
   }
 
-  return(list(snps_per_vmr_counts = snps_per_vmr_counts,
-              VMRs_df_with_cisSNPs = VMRs_df_with_cisSNPs))
+  #### Get the number of overlaps per extended VMR ####
+  VMRs_df_with_cisSNPs$surrounding_SNPs =  GenomicRanges::countOverlaps(VMRs_extended, genot_gr)
+
+  ####Identify the SNPs that are present in each VMR ####
+  snps_per_vmr_find =  GenomicRanges::findOverlaps(VMRs_extended, genot_gr, select = "all")
+  rownames(genotype_information) = genotype_information$Name
+  VMRs_df_with_cisSNPs = VMRs_df_with_cisSNPs %>%
+    dplyr::mutate(SNP = sapply(snps_per_vmr_find, map_revmap_names, genotype_information))
+
+  return(VMRs_df_with_cisSNPs)
 }
