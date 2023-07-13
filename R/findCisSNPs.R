bf842d7957f69e3a6a1a14e89279fc630814a745
Erick Navarro <ericknadel98@ciencias.unam.mx>
2023-07-13 17:49:40 -0600

    Improved documentation of the functions

--- R/findCisSNPs.R
+++ R/findCisSNPs.R
@@ -1,29 +1,34 @@
-#' Find cis SNPs around a set of Variable Methylated Regions
+#' Find cis SNPs around a set of Variable Methylated Regions (VMRs)
 #'
-#' Identification of genotyped SNPs close to each VMR using a distance threshold. **Important**: please make sure that the
-#' positions of the VMR data frame and the ones in the genotype information are from the same genome build.
+#' Identification of genotyped Single Nucleotide Polymorphisms (SNPs) close to each VMR using a distance threshold.
+#'
+#' **Important**: please make sure that the positions of the VMR data frame and the ones in the genotype information are from the same genome build.
 #'
 #' @param VMRs_df A GRanges object converted to a data frame. Must contain the following columns:
-#' "seqnames", "start", "end". These columns are present automatically when doing the object conversion.
+#' "seqnames", "start", "end". These columns are present automatically when doing the object conversion and correspond to the chromosome number, and range of the region.
 #' @param genotype_information A data frame with information about genotyped sites of interest. It must contain the following
-#' columns: "Chr" - chromosome number, "Position" - Genomic basepair position of SNP in the corresponding
-#' chromosome (must contain values of class int), and "Name" - SNP ID.
-#' @param distance The distance threshold to be used to identify cis SNPs. Default is 1 Mb
+#' columns: "CHROM" - chromosome number, "POS" - Genomic basepair position of SNP in the corresponding
+#' chromosome (must contain values of class int), and "ID" - SNP ID. The nomenclature of CHROM must match with the one used in the VMRs_df seqnames column (i.e., if VMRs_df$seqnames uses 1, 2, 3, X, Y or Chr1, Chr2, Chr3, ChrX, ChrY, etc. as chromosome number, the genotype_information$CHROM values must be encoded in the same way).
+#' @param distance The distance threshold to be used to identify cis SNPs. Default is 1 Mb.
 #'
-#' @return a VMR_df object (A data frame compatible with GRanges conversion) with the following new columns: the cis SNPs identified for each VMR, the number
-#' of SNPs surrounding each VMR in the specified window, and VMR_index, which is created if not
-#' already existing based on the rownames of the VMR_df.
+#' @return A VMR_df object (a data frame compatible with GRanges conversion) with the following new columns:
+#'  - The cis SNPs identified for each VMR, the number of SNPs surrounding each VMR in the specified window
+#'  - VMR_index, which is created if not already existing based on the rownames of the VMR_df.
 #' @export
 
 findCisSNPs = function(VMRs_df, genotype_information, distance = 1e6){
+  #Check arguments
+  if(!all(c("seqnames","start","end") %in% colnames(VMRs_df))) stop("Please make sure the VMRs_df object has the required columns with the appropiate names (check documentation for further information)")
+  if(!all(c("CHROM","POS","ID") %in% colnames(genotype_information))) stop("Please make sure the genotype_information object has the required columns with the appropiate names (check documentation for further information)")
+  message("Important: please make sure that the positions of the VMR data frame and the ones in the genotype information are from the same genome build.")
   #Convert VMR and snp data into a GenomicRanges object
   VMRs_gr = GenomicRanges::makeGRangesFromDataFrame(VMRs_df, keep.extra.columns = TRUE)
-  genotype_information = genotype_information %>% dplyr::arrange(Chr) #important step for using Rle later when constructing the GenomicRanges object!
-  seqnames_gr = table(genotype_information$Chr)
+  genotype_information = genotype_information %>% dplyr::arrange(CHROM) #important step for using Rle later when constructing the GenomicRanges object!
+  seqnames_gr = table(genotype_information$CHROM)
   genot_gr = GenomicRanges::GRanges(
     seqnames =  S4Vectors::Rle(names(seqnames_gr), as.numeric(seqnames_gr)), #Number of chromosome; as.numeric to convert from table to numeric vector
-    ranges = IRanges::IRanges(genotype_information$Position, end = genotype_information$Position ,
-                              names = genotype_information$Name))
+    ranges = IRanges::IRanges(genotype_information$POS, end = genotype_information$POS ,
+                              names = genotype_information$ID))
   #Extend each VMR 1 Mb up and downstream
   VMRs_extended = VMRs_gr + distance
 
@@ -38,7 +43,7 @@ findCisSNPs = function(VMRs_df, genotype_information, distance = 1e6){
 
   ####Identify the SNPs that are present in each VMR ####
   snps_per_vmr_find =  GenomicRanges::findOverlaps(VMRs_extended, genot_gr, select = "all")
-  rownames(genotype_information) = genotype_information$Name
+  rownames(genotype_information) = genotype_information$ID
   VMRs_df_with_cisSNPs = VMRs_df_with_cisSNPs %>%
     dplyr::mutate(SNP = sapply(snps_per_vmr_find, map_revmap_names, genotype_information))
 
