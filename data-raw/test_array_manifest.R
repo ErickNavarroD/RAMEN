8eaef89ae95bfdcfeab3ee50eb3685ae77770b80
Erick Navarro <ericknadel98@ciencias.unam.mx>
2023-11-08 20:40:48 +0000

    Fixed source file

--- data-raw/test_array_manifest.R
+++ data-raw/test_array_manifest.R
@@ -1,14 +1,18 @@
 ## code to prepare `test_array_manifest`
-fData_EPIC = read_csv("https://webdata.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v-1-0-b4-manifest-file-csv.zip")
+temp <- tempfile()
+download.file("https://webdata.illumina.com/downloads/productfiles/methylationEPIC/infinium-methylationepic-v-1-0-b4-manifest-file-csv.zip",temp, mode="wb")
+unzip(temp)
+fData_EPIC <- read_csv("MethylationEPIC_v-1-0_B4.csv",
+                       skip = 7)
 array_manifest = fData_EPIC %>%
   dplyr::mutate(STRAND = rep(BiocGenerics::strand("+"), nrow(fData_EPIC))) %>%
-  dplyr::select(MAPINFO, CHR, TargetID, STRAND)
+  dplyr::select(MAPINFO, CHR, IlmnID, STRAND)
 
 #Get the first 3k probes of the 21 chromosome
 test_array_manifest = array_manifest %>%
   filter(CHR == "21") %>%
   arrange(as.numeric(MAPINFO)) %>%
   slice_head(n = 3000) %>%
-  select(-TargetID) #Remove this column because it takes a lot of space when saving the object, and it is already present in the rownames
+  select(-IlmnID) #Remove this column because it takes a lot of space when saving the object, and it is already present in the rownames
 
 usethis::use_data(test_array_manifest, overwrite = TRUE)
